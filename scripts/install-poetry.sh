6a285ddf8978981171347d4ec973ccf74db61f5f
Cong <chicong.v@gmail.com>
2020-09-07 09:25:21 +0900

    Remove package loguru-caplog

--- scripts/install-poetry.sh
+++ scripts/install-poetry.sh
@@ -1,6 +1,5 @@
 set -ex
 
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_HOME=/opt/poetry python && \
-cd /usr/local/bin && \
-ln -s /opt/poetry/bin/poetry && \
+curl -SL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | POETRY_HOME=/opt/poetry python && ln -s /opt/poetry/bin/poetry /usr/local/bin/
+
 poetry config virtualenvs.create false
--- tests/test_model.py
+++
@@ -1,7 +0,0 @@
-from loguru_caplog import loguru_caplog as caplog
-from lib.model import add
-
-
-def test_add(caplog):
-    assert add([-1, 3]) == 2
-    assert "Oh no" in caplog.text
